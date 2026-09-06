2a8feb4f020da82a7693dea6c17dfe4a07d089bb
Kavi <kubahnabawiofficialshop@gmail.com>
2026-09-06 18:10:10 +0700

    konfigurasi

--- firebase-config.js
+++ firebase-config.js
@@ -25,12 +25,12 @@
 // ============================================================
 
 window.FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "AIzaSyC6ea0-3QFnFR3ZUfv4fKYSIX4vTD-ZCwA",
+  authDomain: "kubah-admin-app.firebaseapp.com",
+  projectId: "kubah-admin-app",
+  storageBucket: "kubah-admin-app.firebasestorage.app",
+  messagingSenderId: "477747088600",
+  appId: "1:477747088600:web:9d6d6e6e3fc56bfbab9bad"
 };
 
 // ------------------------------------------------------------
